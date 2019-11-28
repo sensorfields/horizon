31f3c54a1cb5e2de225f197da75a08c55a3811d2
Miko Väli <mikovali@gmail.com>
2019-11-28 23:00:12 +0200

    Initial commit

---
+++ horizon/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk7"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+}
---
+++ horizon/src/main/kotlin/com/sensorfields/horizon/Horizon.kt
@@ -0,0 +1,23 @@
+package com.sensorfields.horizon
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class Horizon<State, Event, Effect, Action> {
+
+    fun dispatch(event: Event) {}
+
+    fun states(): Flow<State> = flow {}
+
+    fun effects(): Flow<Effect> = flow { }
+}
+
+data class Result<State, Effect, Action>(
+    val state: State? = null,
+    val effects: List<Effect> = listOf(),
+    val actions: List<Action> = listOf()
+)
+
+typealias Combine<State, Event, Effect, Action> = (State, Event) -> Result<State, Effect, Action>
+
+typealias Transform<Action, Event> = (Action) -> Flow<Event>

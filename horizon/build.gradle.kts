cdc3c0a1d832c916c10603184cef577473c531be
Miko Väli <mikovali@gmail.com>
2019-12-05 10:32:26 +0200

    Testing

--- horizon/build.gradle.kts
+++ horizon/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     kotlin("jvm") version "1.3.61"
 }
 
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
 repositories {
     jcenter()
 }
@@ -9,4 +15,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk7"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("com.google.truth:truth:1.0")
 }
